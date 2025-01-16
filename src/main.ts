0b542df86b503d410f82991410454577d87e30dd
Artem Bondar <162103655+artem1dev@users.noreply.github.com>
2025-01-16 19:39:12 +0200

    Update README.md and added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import "dotenv/config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.enableCors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    });
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
